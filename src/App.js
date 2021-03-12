f606c7d564cb11fa0d9d6bd40a2b46a3df862e04
Iwan Sutandi <sutandi@student.spb.ac.id>
2021-03-12 22:15:08 +0800

    reinitial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, Input, InputLabel, Container } from '@material-ui/core';
+import Todo from './Todo';
 import './App.css';
+import db from './firebase';
+import firebase from 'firebase';
+
 
 function App() {
+
+  // define state with two variable, todos is model, setTodos is func to modify data model.
+  // once page reload state will erased to new
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+ 
+
+  // when the app loads, listen to database and fetch to new todos as add,edit,delete
+  useEffect(() => {
+    
+    // snapshot : take snap in database and shot in state
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      // set todos data, return string
+      // const data = snapshot.docs.map(doc => ({id: doc.id, todo: doc.data()}));
+
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data()})));
+
+    });
+
+
+  }, []);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    db.collection('todos').add({
+      text: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    // ...todos define next array, input is new text in array
+    // setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World {input}</h1>
+      <form>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Input Todo</InputLabel>
+          <Input id="my-input" value={input} onChange={event => setInput(event.target.value)} />
+          <Button type="submit" onClick={addTodo} variant="contained" color="primary" disabled={!input}>
+            addTodo
+          </Button>
+        </FormControl>
+      </form>
+      <Container>
+        <>
+        {todos.map((todo) => (
+         
+            <Todo todo={todo} key={todo.id} />
+          
+        ))}
+        </>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
